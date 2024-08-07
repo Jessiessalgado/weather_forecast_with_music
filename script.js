2097d6dfe1336f6b0d035361d79786cf2131c8d3
Jessica Salgado <jessica.silvasalgado@icloud.com>
2024-08-07 20:42:24 -0300

    add the input-search function

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function searchButton(){
-  const input = document.getElementById('input-search');  
+function searchButton() {
+    const input = document.getElementById('input-search');
+}
+
+function closeInput() {
+    document.getElementById('input-search').style.visibility = 'hidden';
+    document.getElementById('input-search').style.width = '40px';
+    document.getElementById('input-search').style.padding = '0.5rem 0.5rem 0.5rem 2.6rem';
+}
+
+function openInput() {
+    document.getElementById('input-search').style.visibility = 'visible';
+    document.getElementById('input-search').style.width = '300px';
+    document.getElementById('input-search').style.padding = '0.5rem 0.5rem 0.5rem 3.1rem';
 }
